c62141bad1d0ac75b0e18611cd81a4a38775ab99
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-02-20 07:56:11 +0700

    modified exercise 6

--- p0-w3-ex6.js
+++ p0-w3-ex6.js
@@ -1,23 +1,17 @@
 function angkaPalindrome(num) {    
-    var counter = num
     var num2 = false
-    
     while(num2 === false) {
-        counter++
-        var angkaString = counter.toString()
+        num++
+        var angkaString = num.toString()
         var temp = ''
         for(var i = angkaString.length - 1; i >= 0; i--) {
             temp += angkaString[i]
             if(temp === angkaString) {
                 return Number(temp)
             }
-            else {
-                num2 = false
-            }
         }
     }
 }
-  
   // TEST CASES
   console.log(angkaPalindrome(8)); // 9
   console.log(angkaPalindrome(10)); // 11
