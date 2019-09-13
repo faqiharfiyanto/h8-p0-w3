a5ecacd533357cb419ca097095c6308f3d5bd3da
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-09-13 16:28:24 +0700

    modifying exercise 3

--- p0-w3-ex3.js
+++ p0-w3-ex3.js
@@ -1,14 +1,14 @@
 var input = [
     ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
     ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
-    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0003", "Winona", "Ambon", "32/12/1965", "Memasak"],
     ["0004", "Bintang Senjaya", "Martapura", "6/13/1970", "Berkebun"]
 ]
 function dataHandling(a) {
     for (var i=0; i<a.length; i++) {
         var bulan = a[i][3].split('/')
-        if (bulan[1]>12) {
-            console.log('Maaf tanggal lahir salah')
+        if (bulan[1]>12 || bulan[0]>31) {
+            console.log('Maaf, input tanggal lahir salah')
         } else {
             var result="Nomor ID: "+a[i][0]+"\n"+"Nama Lengkap: "+a[i][1]+"\n"+"TTL: "+a[i][2]+", "+a[i][3]+"\n"+"Hobi: "+a[i][4]+"\n"
             console.log(result)
